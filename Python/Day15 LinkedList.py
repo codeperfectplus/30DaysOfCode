57e21e8f5cdc74ff72c66f9499d8c0ab887c805b
Deepak Raj <54245038+perfect104@users.noreply.github.com>
2020-04-29 22:47:57 +0530

    Naming convention changed

--- Python/Day15 LinkedList.py
+++ Python/Day15 LinkedList.py
@@ -11,7 +11,15 @@ class Solution:
 
     def insert(self,head,data): 
     #Complete this method
-    
+        if head is None:
+            head = Node(data)
+        else:
+            curr = head
+            while curr.next:
+                curr = curr.next
+            curr.next = Node(data)
+        return head
+        
 mylist= Solution()
 T=int(input())
 head=None
